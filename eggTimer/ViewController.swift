cc2e0ebaa519b14a5e4bd9fd59992e7a34f94f99
phoebeCauld <vikaperova291013@gmail.com>
2021-09-03 23:31:58 +0300

    change timer value

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -6,15 +6,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-   private let typesOfEggsArray = ["soft","medium","hard"]
-   private let hardness = ["soft":10, "medium":480, "hard":720]
-   private var counter = 0
-   private var timer = Timer()
+    private let typesOfEggsArray = ["soft","medium","hard"]
+    private let hardness = ["soft":300, "medium":480, "hard":720]
+    private var counter = 0
+    private var player = AVAudioPlayer()
+    private var timer = Timer()
+    private let timerLabel = UILabel()
+    private let shapeLayer = CAShapeLayer()
+    private let trackLayer = CAShapeLayer()
+    
+    private let topView = UIView()
+    private let centerView = UIStackView()
+    private let bottomView = UIView()
+    
     let titleLabel: UILabel = {
-       let titleLabel = UILabel()
+        let titleLabel = UILabel()
         titleLabel.text = "How do you like your eggs?"
         titleLabel.textAlignment = .center
         titleLabel.adjustsFontSizeToFitWidth = true
@@ -23,7 +33,7 @@ class ViewController: UIViewController {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         return titleLabel
     }()
-  
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,10 +41,11 @@ class ViewController: UIViewController {
         addSubViewsOnViewController()
         setViews()
     }
-    
-    private let topView = UIView()
-    private let bottomView = UIView()
-    private let centerView = UIStackView()
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    // add circle in center of bottomView
+        self.animationCircle()
+    }
     
     func addSubViewsOnViewController(){
         self.view.addSubview(topView)
@@ -49,7 +60,6 @@ class ViewController: UIViewController {
     }
     
     func configureTopView(){
-        
         topView.addSubview(titleLabel)
         
         topView.translatesAutoresizingMaskIntoConstraints = false
@@ -77,7 +87,6 @@ class ViewController: UIViewController {
     }
     
     func configureBottomView(){
-        bottomView.backgroundColor = .cyan
         bottomView.translatesAutoresizingMaskIntoConstraints = false
         bottomView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         bottomView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
@@ -85,37 +94,36 @@ class ViewController: UIViewController {
         bottomView.heightAnchor.constraint(equalTo: self.view.heightAnchor,
                                            multiplier: Constants.multiply).isActive = true
         
-        let shadowView = UIView()
-        bottomView.addSubview(shadowView)
-        shadowView.translatesAutoresizingMaskIntoConstraints = false
-        shadowView.centerXAnchor.constraint(equalTo: bottomView.centerXAnchor).isActive = true
-        shadowView.centerYAnchor.constraint(equalTo: bottomView.centerYAnchor).isActive = true
-        shadowView.backgroundColor = .white
-        
-        let shapeLayer = CAShapeLayer()
-        let center = CGPoint(x: shadowView.frame.size.width, y: shadowView.frame.size.height)
-        let circularPath = UIBezierPath(arcCenter: center, radius: 50, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        print(center)
-        shapeLayer.path = circularPath.cgPath
-        
-        shadowView.layer.addSublayer(shapeLayer)
+        timerLabel.text = ""
+        timerLabel.font = UIFont.systemFont(ofSize: 30)
+        timerLabel.textColor = .systemGray
+        timerLabel.textAlignment = .center
+        timerLabel.numberOfLines = 0
+        bottomView.addSubview(timerLabel)
+        timerLabel.translatesAutoresizingMaskIntoConstraints = false
+        timerLabel.centerXAnchor.constraint(equalTo: bottomView.centerXAnchor).isActive = true
+        timerLabel.centerYAnchor.constraint(equalTo: bottomView.centerYAnchor).isActive = true
     }
     
     func addEggButtons(){
         for i in 0...typesOfEggsArray.count - 1 {
+// create egg images
             let eggImage = UIImageView(image: UIImage(named: typesOfEggsArray[i]))
             eggImage.translatesAutoresizingMaskIntoConstraints = false
             eggImage.contentMode = .scaleAspectFit
+// create buttons for images
             let button = UIButton()
             button.setTitle(typesOfEggsArray[i], for: .normal)
             button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
             button.setTitleColor(.systemGray, for: .normal)
             button.addTarget(self, action: #selector(eggButtonPressed), for: .touchUpInside)
             button.translatesAutoresizingMaskIntoConstraints = false
+// create subviews for images and buttons
             let eggView = UIView()
             self.view.addSubview(eggView)
             eggView.addSubview(eggImage)
             eggView.addSubview(button)
+// set constraints
             eggView.translatesAutoresizingMaskIntoConstraints = false
             eggImage.centerXAnchor.constraint(equalTo: eggView.centerXAnchor).isActive = true
             eggImage.centerYAnchor.constraint(equalTo: eggView.centerYAnchor).isActive = true
@@ -132,18 +140,65 @@ class ViewController: UIViewController {
         guard let senderTitle = sender.currentTitle else { return }
         guard let secondsForCooking = hardness[senderTitle] else { return }
         counter = secondsForCooking
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-        
+        timerLabel.text = "\(counter)"
+        animationTimer()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self,
+                                     selector: #selector(self.updateCounter), userInfo: nil, repeats: true)
     }
-
+    
     @objc func updateCounter(){
-        if counter > 0 {
-            print("\(counter) seconds")
-                    counter -= 1
-        } else {
+        if counter > 1 {
+            counter -= 1
+            timerLabel.text = "\(counter)"
+        } else if counter == 1 {
             timer.invalidate()
-            titleLabel.text = "DONE"
+            timerLabel.text = "DONE"
+            titleLabel.text = "Enjoy your meal!"
+            alarmSound()
         }
+    }
+    
+    private func alarmSound(){
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func animationTimer(){
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 0
+        basicAnimation.duration = CFTimeInterval(counter)
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = true
+        shapeLayer.add(basicAnimation, forKey: "basicAnimation")
+    }
+    
+    private func animationCircle(){
+        let center = CGPoint(x: bottomView.frame.width / 2, y: bottomView.frame.height / 2)
+        let endAngle = (-CGFloat.pi / 2)
+        let startAngle = 2 * CGFloat.pi + endAngle
+        let circularPath = UIBezierPath(arcCenter: center, radius: 50,
+                                        startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        trackLayer.path = circularPath.cgPath
+        shapeLayer.path = circularPath.cgPath
+        
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.lineCap = CAShapeLayerLineCap.round
+        
+        shapeLayer.strokeColor = UIColor.yellow.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineCap = CAShapeLayerLineCap.round
+        shapeLayer.strokeEnd = 1
+        
+        bottomView.layer.addSublayer(trackLayer)
+        bottomView.layer.addSublayer(shapeLayer)
     }
 }
 
